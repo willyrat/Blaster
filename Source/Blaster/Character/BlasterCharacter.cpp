81600656fc5d954e447afb040bc9b84f879be15e
Bill Pratt <bill_pratt@rrmac.com>
2024-08-10 02:48:40 -0400

    setup enahanced input in c++ for move and mouse look

--- Source/Blaster/Character/BlasterCharacter.cpp
+++ Source/Blaster/Character/BlasterCharacter.cpp
@@ -4,6 +4,10 @@
 #include "BlasterCharacter.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "InputMappingContext.h"
+#include "InputActionValue.h"
 
 // Sets default values
 ABlasterCharacter::ABlasterCharacter()
@@ -27,7 +31,59 @@ void ABlasterCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	;
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		// Get the local player subsystem
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			// Clear out existing mapping, and add our mapping
+			Subsystem->ClearAllMappings();
+			Subsystem->AddMappingContext(BlasterInputMapping, 0);
+		}
+	}
+
 }
+
+void ABlasterCharacter::Move(const FInputActionValue& Value)
+{
+	//https://www.youtube.com/watch?v=n3n8bqu7F9Q
+	// 
+	//const float DirectionValue = Value.Get<float>();
+
+	
+	const FVector2D MovementVector = Value.Get<FVector2D>();
+
+	
+	UE_LOG(LogTemp, Warning, TEXT("IA_Move triggered"));
+				
+	const FRotator Rotation = Controller->GetControlRotation();
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+	
+	const FVector ForwardDirectoin = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);  
+	AddMovementInput(ForwardDirectoin, MovementVector.Y);
+	FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);  
+	AddMovementInput(RightDirection, MovementVector.X);
+		 
+}
+
+void ABlasterCharacter::Look(const FInputActionValue& Value)
+{
+	const FVector2D LookAxisValue = Value.Get<FVector2D>();
+
+	if (Controller)
+	{
+		AddControllerYawInput(LookAxisValue.X);
+		AddControllerPitchInput(LookAxisValue.Y);
+	}
+
+}
+
+void ABlasterCharacter::Jump()
+{
+	Super::Jump();
+}
+
 
 // Called every frame
 void ABlasterCharacter::Tick(float DeltaTime)
@@ -41,5 +97,16 @@ void ABlasterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCo
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ABlasterCharacter::Move);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ABlasterCharacter::Look);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ABlasterCharacter::Jump);
+
+	}
+
+
 }
+
+
 
--- Source/Blaster/Character/BlasterCharacter.h
+++ Source/Blaster/Character/BlasterCharacter.h
@@ -4,7 +4,15 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputActionValue.h"
+
+
 #include "BlasterCharacter.generated.h"
+
+//try to forward declare when you can when in .h files
+//forward declare
+//pointers you can forward declare, but structs you cannot... structs are generally small so should be safe...see Move function below
+class UInputMappingContext;
 
 
 UCLASS()
@@ -20,10 +28,32 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	virtual void  Jump() override;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Enhanced Input")
+	UInputMappingContext* BlasterInputMapping;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Enhanced Input")
+	class UInputAction* MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Enhanced Input")
+	class UInputAction* LookAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Enhanced Input")
+	class UInputAction* JumpAction;
+	
+	//use this to add a new input action
+	//UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Enhanced Input")
+	//class UInputAction* LookAction;
+	
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+	
+
 
 private:
 	UPROPERTY(VisibleAnywhere, Category = Camera)
@@ -31,6 +61,7 @@ private:
 
 	UPROPERTY(VisibleAnywhere, Category = Camera)
 	class UCameraComponent* FollowCamera;
+		
 
 public:
 	//getters and setters
