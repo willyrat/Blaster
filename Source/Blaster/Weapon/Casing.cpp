c53fe2dc6411ddc2f5638c7a8831cafdb237b25b
Bill Pratt <bill_pratt@rrmac.com>
2024-08-20 00:59:16 -0400

    lesson 79 added in shell physics and sound... did challenge to delay casing destory

--- Source/Blaster/Weapon/Casing.cpp
+++ Source/Blaster/Weapon/Casing.cpp
@@ -2,6 +2,9 @@
 
 
 #include "Casing.h"
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
+#include "TimerManager.h"
 
 // Sets default values
 ACasing::ACasing()
@@ -10,7 +13,13 @@ ACasing::ACasing()
 	PrimaryActorTick.bCanEverTick = false;
 
 	CasingMesh = CreateDefaultSubobject <UStaticMeshComponent>(TEXT("CasingMesh"));
+	CasingMesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera, ECollisionResponse::ECR_Ignore);
+	CasingMesh->SetSimulatePhysics(true);
+	CasingMesh->SetEnableGravity(true);
+	CasingMesh->SetNotifyRigidBodyCollision(true);	//in blueprint this is called Simulation Generates Hit Events... make sure this is checked in bp if set true here
+	ShellEjectionImpules = 10.f;
 
+	DelayDestory = 3.f;
 }
 
 // Called when the game starts or when spawned
@@ -18,7 +27,38 @@ void ACasing::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	CasingMesh->AddImpulse(GetActorForwardVector()* ShellEjectionImpules);
+
+	if (HasAuthority())
+	{
+		CasingMesh->OnComponentHit.AddDynamic(this, &ACasing::OnHit);
+	}
 }
+
+
+
+void ACasing::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (ShellSound && !TimerHandle.IsValid())
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, ShellSound, GetActorLocation());
+	}
+
+	GetWorldTimerManager().SetTimer(TimerHandle, this, &ACasing::DestroyMe, DelayDestory, false, -1.f);
+
+	//Destroy();
+}
+
+void ACasing::DestroyMe()
+{
+	TimerHandle.Invalidate();
+	Destroy();
+}
+
+
+
+
+
 
 
 
--- Source/Blaster/Weapon/Casing.h
+++ Source/Blaster/Weapon/Casing.h
@@ -15,13 +15,31 @@ public:
 	// Sets default values for this actor's properties
 	ACasing();
 
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	
+
 private:
 	UPROPERTY(VisibleAnywhere)
 	UStaticMeshComponent* CasingMesh;
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+	UPROPERTY(EditAnywhere)
+	float ShellEjectionImpules;
+
+	//when we bind to overlap and hit functions they have to be ufunctions
+	UFUNCTION()
+	virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+	UPROPERTY(EditAnywhere)
+	class USoundCue* ShellSound;
+
+	//for challenge
+	FTimerHandle TimerHandle;
+	UPROPERTY(EditAnywhere)
+	float DelayDestory;
+	void DestroyMe();
 
 public:	
 	
