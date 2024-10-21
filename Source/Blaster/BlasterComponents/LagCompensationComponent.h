52898118b7016c6683a658e5cb9d49fcdcf9457f
Bill Pratt <bill_pratt@rrmac.com>
2024-10-21 11:14:03 -0400

    lesson 187 ... adding in LagCompenstaionComponent class

---
+++ Source/Blaster/BlasterComponents/LagCompensationComponent.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "LagCompensationComponent.generated.h"
+
+//this class handles server side rewind
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BLASTER_API ULagCompensationComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	ULagCompensationComponent();
+
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+
+public:	
+	
+		
+};
